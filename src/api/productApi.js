8ace9b613bdd784eea31b3d5ebff8edeb067f349
akasrai <akasky70@gmail.com>
2025-05-26 21:11:46 +0545

    add react hook form and yup package

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -3,7 +3,21 @@ import axios from "axios";
 export const fetchProducts = async () => {
   const response = await axios({
     method: "get",
-    url: "https://dummyjson.com/products",
+    url: "https://dummyjson.com/products?sortBy=title&order=asc",
+  });
+
+  console.log(response.data);
+  return response.data;
+};
+
+export const addProduct = async (product) => {
+  const response = await axios({
+    method: "post",
+    url: "https://dummyjson.com/products/add",
+    data: product,
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
 
   return response.data;
--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,6 +1,49 @@
+import { string, object } from 'yup';
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+
+import { addProduct } from "../api/productApi";
+
+const productSchema = object({
+    title: string()
+        .required("Title is required.")
+        .min(5, "Title must be at least 3 characters long.")
+        .max(50, "Title must be at most 50 characters long.")
+});
+
 const AddProduct = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(productSchema),
+    });
+
+    const onSubmit = async (data) => {
+        const response = await addProduct(data);
+
+        if (response) {
+            alert("Product added successfully");
+        } else {
+            alert("Failed to add product");
+        }
+    }
+
     return (
-        <div>Add product</div>
+        <div>
+            <div className="flex ">
+                <h1 className="">Add Product</h1>
+
+            </div>
+            <form onSubmit={handleSubmit(onSubmit)} className="flex-col" >
+                <input {...register("title")} />
+
+                {errors.title && <span>{errors.title?.message}</span>}
+
+                <input type="submit" />
+            </form>
+        </div>
     )
 }
 

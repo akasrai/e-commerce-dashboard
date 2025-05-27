552caf3a77492fa9a29fa3e6173cd9506c996088
akasrai <akasky70@gmail.com>
2025-05-27 21:21:18 +0545

    Added form add product

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router";
+import { ToastContainer } from 'react-toastify';
+
 
 import AppRoutes from './Routes.jsx'
 
@@ -10,6 +12,7 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <StrictMode>
       <AppRoutes />
+      <ToastContainer />
     </StrictMode>
   </BrowserRouter>
 );
--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,5 +1,6 @@
-import { string, object } from 'yup';
-import { useForm } from "react-hook-form"
+import { toast } from 'react-toastify';
+import { useForm } from "react-hook-form";
+import { string, object, number } from 'yup';
 import { yupResolver } from "@hookform/resolvers/yup"
 
 import { addProduct } from "../api/productApi";
@@ -8,14 +9,30 @@ const productSchema = object({
     title: string()
         .required("Title is required.")
         .min(5, "Title must be at least 3 characters long.")
-        .max(50, "Title must be at most 50 characters long.")
+        .max(50, "Title must be at most 50 characters long."),
+
+    description: string()
+        .required("Description is required.")
+        .min(5, "Description must be at least 10 characters long.")
+        .max(50, "Description must be at most 100 characters long."),
+
+    price: number()
+        .required("Price is required.")
+        .positive("Price must be a positive number.")
+        .min(0, "Price must be at least 1.")
+        .max(100000, "Price must be at most 10000."),
+
+    brand: string()
+        .required("Brand is required.")
+        .oneOf(["Apple", "Samsung", "Sony", "LG", "Nokia"], "Brand must be one of the predefined options.")
 });
 
 const AddProduct = () => {
     const {
         register,
+        reset,
         handleSubmit,
-        formState: { errors },
+        formState: { errors, isSubmitting },
     } = useForm({
         resolver: yupResolver(productSchema),
     });
@@ -23,10 +40,12 @@ const AddProduct = () => {
     const onSubmit = async (data) => {
         const response = await addProduct(data);
 
+        reset({ price: 0 });
+
         if (response) {
-            alert("Product added successfully");
+            toast.success("Product added successfully");
         } else {
-            alert("Failed to add product");
+            toast.error("Failed to add product");
         }
     }
 
@@ -34,14 +53,49 @@ const AddProduct = () => {
         <div>
             <div className="flex ">
                 <h1 className="">Add Product</h1>
-
             </div>
-            <form onSubmit={handleSubmit(onSubmit)} className="flex-col" >
-                <input {...register("title")} />
+            <form onSubmit={handleSubmit(onSubmit)} className="flex-col add-product-form" >
+                <div className='form-control flex-col'>
+                    <label>Title*</label>
+                    <input className={`${errors.title && "is-invalid"}`} placeholder="Enter product title" type="text" {...register("title")} />
 
-                {errors.title && <span>{errors.title?.message}</span>}
+                    {errors.title && <span className='error-message'>{errors.title?.message}</span>}
+                </div>
 
-                <input type="submit" />
+                <div className='form-control flex-col'>
+                    <label>Description*</label>
+                    <input className={`${errors.description && "is-invalid"}`} placeholder="Enter product description" type="text" {...register("description")} />
+
+                    {errors.description && <span className='error-message'>{errors.description?.message}</span>}
+                </div>
+
+                <div className='form-control flex-col'>
+                    <label>Price*</label>
+                    <input className={`${errors.price && "is-invalid"}`} placeholder="Enter product price" defaultValue={0} type="number" {...register("price")} />
+
+                    {errors.price && <span className='error-message'>{errors.price?.message}</span>}
+                </div>
+
+                <div className='form-control flex-col'>
+                    <label>Brand*</label>
+
+                    <select className={`${errors.brand && "is-invalid"}`} {...register("brand")}>
+                        <option value="">Select a brand</option>
+                        <option value="Apple">Apple</option>
+                        <option value="Samsung">Samsung</option>
+                        <option value="Sony">Sony</option>
+                        <option value="LG">LG</option>
+                        <option value="Nokia">Nokia</option>
+                    </select>
+
+                    {errors.brand && <span className='error-message'>{errors.brand?.message}</span>}
+                </div>
+
+                <button className="" type="submit" disabled={isSubmitting}>
+                    {
+                        isSubmitting ? "Adding..." : "Add Product"
+                    }
+                </button>
             </form>
         </div>
     )
